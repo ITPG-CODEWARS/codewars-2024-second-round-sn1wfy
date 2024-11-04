a7a789f0826ebd6e88e1e3fec6ea55587912363d
sn1wfy <martinnikolov688@gmail.com>
2024-11-04 18:12:20 +0200

    Creation of the asp.net app.

---
+++ ShortIt/ShortIt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShortIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
